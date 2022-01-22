743ea0117b0a04090b09700303d907875ef9659a
Oliver Schurius <mail@schurius.de>
2022-01-22 16:23:43 +0100

    some code cleaning, improved pylint result > 7

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -9,7 +9,7 @@ import wandb
 import os
 
 
-#logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
@@ -28,18 +28,22 @@ def go(args):
     df = pd.read_csv(artifact_path)
 
     # Drop outliers
-    logger.info("Dropping the outliers")
+    logger.info("Dropping the outliers with a defined price range")
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
     # Convert last_review to datetime
-    logger.info("Converting column format")
+    logger.info("Converting column last_review to date format")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
-    # Assuming that in production all names and hostnames are filled properly set it to an empty string
-    df['name'].fillna("", inplace=True)
-    df['host_name'].fillna("", inplace=True)
+    # Assuming that in production all names and hostnames are filled properly
+    # set it to an empty string
+    df['name'].fillna("dummy", inplace=True)
+    df['host_name'].fillna("dummy", inplace=True)
+    logger.info(
+        f"Removed missing values for columns name and host_name (NaN={df['name'].isnull().values.any()})")
 
+    # upload cleand data as an artifact to WB
     logger.info("Storing cleaned data")
     df.to_csv(args.output_artifact)
 
@@ -52,7 +56,10 @@ def go(args):
 
     logger.info("Logging artifact")
     run.log_artifact(artifact)
+    artifact.wait()
 
+    # clean up
+    logger.info("Clean up temp file")
     os.remove(args.output_artifact)
 
 
@@ -61,42 +68,42 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     parser.add_argument(
-        "--input_artifact", 
+        "--input_artifact",
         type=str,
         help="raw date to be processed",
         required=True
     )
 
     parser.add_argument(
-        "--output_artifact", 
+        "--output_artifact",
         type=str,
         help="cleaned data stored in WB",
         required=True
     )
 
     parser.add_argument(
-        "--output_type", 
+        "--output_type",
         type=str,
         help="type of the artifact, defaults to cleaned_data",
         required=False
     )
 
     parser.add_argument(
-        "--output_description", 
+        "--output_description",
         type=str,
         help="cleaned data artifact for further processing",
         required=True
     )
 
     parser.add_argument(
-        "--min_price", 
+        "--min_price",
         type=float,
         help="min value for data reduction",
         required=True
     )
 
     parser.add_argument(
-        "--max_price", 
+        "--max_price",
         type=float,
         help="min value for data reduction",
         required=True

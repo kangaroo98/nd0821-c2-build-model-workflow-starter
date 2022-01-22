5d1a13cc0d5f492e538f00800dc8675365579eda
Oliver Schurius <mail@schurius.de>
2022-01-23 00:14:16 +0100

    added train_random_forest  step in main / config

--- main.py
+++ main.py
@@ -105,11 +105,19 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": f"{config['artifacts']['trainval_data_artifact_name']}:latest",
+                    "val_size": config['modeling']['val_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by'],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config['modeling']['max_tfidf_features'],
+                    "output_artifact": config['artifacts']['model_artifact_name']
+                }
+            )
 
         if "test_regression_model" in active_steps:
 
